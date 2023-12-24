418e46314f9a2604943f85062144a46a7760e4e9
Fachrur Razi <fachrurrazi95@gmail.com>
2023-12-25 06:43:45 +0700

    setting up typeorm

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -32,6 +32,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
         },
       }),
       inject: [ConfigService],
+      imports: [ConfigService],
     }),
   ],
   controllers: [AuthController],
